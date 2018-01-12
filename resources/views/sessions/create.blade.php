5cc0d06f1072899c079531318ddbb8e63b01f64a
zhuli <18652968364@163.com>
2018-01-12 06:46:27 +0000

    Add password resets & empty  email configuration

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -12,12 +12,12 @@
             <form action="{{ route('login') }}" method="post">
                 {{ csrf_field() }}
                 <div class="form-group">
-                    <label for="email">邮箱</label>
+                    <label for="email">邮箱:</label>
                     <input type="text" name="email" class="form-control" value="{{ old('email') }}">
                 </div>
 
                 <div class="form-group">
-                    <label for="password">密码</label>
+                    <label for="password">密码（<a href="{{ route('password.request') }}">忘记密码</a>）:</label>
                     <input type="password" name="password" value="{{ old('password')}}" class="form-control">
                 </div>
 
